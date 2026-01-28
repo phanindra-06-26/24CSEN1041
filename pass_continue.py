bd17846aed28e7fc45a8d9a931f90c575610c3ed
phanindra-06-26 <ppindi@student.gitam.edu>
2026-01-28 10:51:47 +0530

    Create pass_continue.py

---
+++ pass_continue.py
@@ -0,0 +1,28 @@
+for i in range(1,20):
+    if i==5:
+        continue
+
+    if i==10:
+        pass
+    print(i)
+##output
+1
+2
+3
+4
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+
+=== Code Execution Successful ===
