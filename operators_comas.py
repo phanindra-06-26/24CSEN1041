e94b1181408373018c4b134638ab9cb7a5ad67a5
phanindra-06-26 <ppindi@student.gitam.edu>
2026-01-29 15:42:02 +0530

    Create operators_comas.py

---
+++ operators_comas.py
@@ -0,0 +1,23 @@
+a = 6
+b = 9
+
+print("Arithmetic Operators")
+print(a, "+", b, "=", a + b)
+print(a, "-", b, "=", a - b)
+print(a, "*", b, "=", a * b)
+print(a, "/", b, "=", round(a / b, 2))
+print(a, "%", b, "=", a % b)
+
+
+
+
+
+####output
+Arithmetic Operators
+6 + 9 = 15
+6 - 9 = -3
+6 * 9 = 54
+6 / 9 = 0.67
+6 % 9 = 6
+
+=== Code Execution Successful ===
