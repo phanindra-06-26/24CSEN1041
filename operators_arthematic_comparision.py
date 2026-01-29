32d41307f91154e102d17520560427f70844346d
phanindra-06-26 <ppindi@student.gitam.edu>
2026-01-29 15:07:36 +0530

    Create operators_arthematic_comparision.py

---
+++ operators_arthematic_comparision.py
@@ -0,0 +1,48 @@
+a = 6
+b = 9
+
+print("Arithmetic Operators")
+print(f"{a} + {b} = {a + b}")
+print(f"{a} - {b} = {a - b}")
+print(f"{a} * {b} = {a * b}")
+print(f"{a} / {b} = {a / b:.2f}")   # Float division
+print(f"{a} // {b} = {a // b}")     # Floor division
+print(f"{a} % {b} = {a % b}")       # Modulus
+
+print("Comparison Operators")
+print(f"{a} == {b} : {a == b}")
+print(f"{a} != {b} : {a != b}")
+print(f"{a} > {b}  : {a > b}")
+print(f"{a} < {b}  : {a < b}")
+print(f"{a} >= {b} : {a >= b}")
+print(f"{a} <= {b} : {a <= b}")
+
+print("Logical Operators")
+print(f"{a} > 5 and {b} < 5 : {a > 5 and b < 5}")
+print(f"{a} > 5 or {b} > 5  : {a > 5 or b > 5}")
+print(f"not({a} > 5)        : {not(a > 5)}")
+print()
+
+###output
+Arithmetic Operators
+6 + 9 = 15
+6 - 9 = -3
+6 * 9 = 54
+6 / 9 = 0.67
+6 // 9 = 0
+6 % 9 = 6
+Comparison Operators
+6 == 9 : False
+6 != 9 : True
+6 > 9  : False
+6 < 9  : True
+6 >= 9 : False
+6 <= 9 : True
+Logical Operators
+6 > 5 and 9 < 5 : False
+6 > 5 or 9 > 5  : True
+not(6 > 5)        : False
+
+
+=== Code Execution Successful ===
+
