8ae071545a0fe88d4a920b872df7c7dc4e7767e6
phanindra-06-26 <ppindi@student.gitam.edu>
2026-02-05 15:29:18 +0530

    Create membership_strings.py

---
+++ membership_strings.py
@@ -0,0 +1,20 @@
+team=("virat","rohit","dhoni","hardik","bumrah","varun","surya","kl rahul","shami","siraj","phani")
+
+print("phani" in team)
+print("thala" not in team)
+print("virat"not in team)
+print("dhoni"in team)
+print("pandya"not in team)
+print()
+
+
+####output
+
+True
+True
+False
+True
+True
+
+
+=== Code Execution Successful ===
